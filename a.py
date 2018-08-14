cb56a9c40dd27f29ff7774622da98ca17f0963de
laertskreli <laertskreli@gmail.com>
2018-08-13 22:29:57 -0500

    first test

---
+++ a.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+account_one = "Chase Credit Card"
+account_one_apr = 0.25
+account_one_balance = 2500
+
+period_interest = np.product() * account_one_balance
+period_two_balance = np.sum(account_one_balance, period_interest)
+
+print(period_two_balance)
